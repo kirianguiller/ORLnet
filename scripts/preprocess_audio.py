db24bf8d874f4bc0946de6aef8e61f9ff3ccad80
wrandraall <kiriangui@gmail.com>
2019-12-04 17:57:13 +0100

    change data

--- scripts/preprocess_audio.py
+++ scripts/preprocess_audio.py
@@ -4,26 +4,33 @@ import os
 import pandas as pd
 
 N_TO_PROCESS = 30
+path_project_data = '../data/'
+path_source_data = "/home/wran/corpus/mozilla_voices/"
 languages = ['ru', 'nl', 'de']
+languages = ['nl']
 
 for lang in languages:
-    path_to_data = '../data/{}_small'.format(lang)
-    path_source_data = "/home/wran/Plurital/full_data_voice_language_recognizer/{}/clips/".format(lang)
-    path_clips = os.path.join(path_to_data, 'clips')
+    path_dest_lang = os.path.join(path_project_data, lang)
+    path_source_lang = os.path.join(path_source_data, lang)
 
-    if not os.path.exists(path_clips):
-        os.makedirs(path_clips)
+    path_source_clips = os.path.join(path_source_lang, 'clips')
 
-    path_validated_tsv = os.path.join(path_to_data, 'validated.tsv')
+    print(path_dest_lang)
+    print(path_source_lang)
+    print(path_source_clips)
+
+    if not os.path.exists(path_dest_lang):
+        os.makedirs(path_dest_lang)
+
+    path_validated_tsv = os.path.join(path_source_lang, 'validated.tsv')
     validated_tsv = pd.read_csv(path_validated_tsv, sep='\t')
 
-    for n in range(N_TO_PROCESS):
-        row = validated_tsv.iloc[n]
-        path_src = os.path.join(path_source_data, row.path)
-        path_dest = os.path.join(path_clips,row.path)
-        path_dest_wav = path_dest.replace('.mp3', '.wav')
-        path_dest_txt = path_dest.replace('.mp3', '.txt')
-        sound = pydub.AudioSegment.from_mp3(path_src)
+    for index, row in validated_tsv.iterrows():
+        path_mp3_src = os.path.join(path_source_clips, row.path)
+        path_mp3_dest = os.path.join(path_dest_lang, row.path)
+        path_dest_wav = path_mp3_dest.replace('.mp3', '.wav')
+        path_dest_txt = path_mp3_dest.replace('.mp3', '.txt')
+        sound = pydub.AudioSegment.from_mp3(path_mp3_src)
         sound.export(path_dest_wav, format='wav')
         with open(path_dest_txt, 'w', encoding='utf-8') as f:
             f.write(row.sentence)
